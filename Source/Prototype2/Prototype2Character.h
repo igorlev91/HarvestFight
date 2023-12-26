be54f3c1e87aa3a52b8c97ef7a714361b5206cc3
Igor Levdansky <igorlevdansky91@gmail.com>
2023-12-26 17:02:16 +0300

    InteractInterface class implemented, more functionality for character

--- Source/Prototype2/Prototype2Character.h
+++ Source/Prototype2/Prototype2Character.h
@@ -5,21 +5,47 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "InputActionValue.h"
+#include "Widgets/Widget_IngameMenu.h"
 #include "Prototype2Character.generated.h"
 
 UCLASS(config=Game)
 class APrototype2Character : public ACharacter
 {
 	GENERATED_BODY()
+public:
+	APrototype2Character();	
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+	// UI
+	//UPROPERTY(EditAnywhere) TSubclassOf<UWidget_IngameMenu> WidgetIngameMenu;
+	//UWidget_IngameMenu* WidgetIngameMenuInstance;
 	
+	void OpenIngameMenu();
+	
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	virtual void BeginPlay();
+
+	virtual void Tick(float DeltaSeconds) override;
+	
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/* Called for Attack input */
+	void ChargeAttack();
+
+	/* Release Attack */
+	void ReleaseAttack();
+
+	/* Pickup/Plant/Sell */
+	void Interact();
+
+	void CheckForInteractables();	
+
+private: 
 	/** MappingContext */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputMappingContext* DefaultMappingContext;
@@ -36,29 +62,43 @@ class APrototype2Character : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputAction* LookAction;
 
-public:
-	APrototype2Character();
-	
+	/* UI */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MenuAction;
 
+	/* Attack */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* AttackAction;
+
+	/* PickUp/Plant/Sell */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* InteractAction;
+
+	/* Drop */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* DropAction;
+
+	UPROPERTY(EditAnywhere)
+	float InteractRadius = 500.0f;
 protected:
+	/* Weapon Held */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class AWeapon* Weapon;
 
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	/* Currently held item */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class APickUpItem* HeldItem;
 	
-	// To add mapping context
-	virtual void BeginPlay();
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
 
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+private:
+	class IInteractInterface* ClosestInteractableItem;
+	bool bIsChargingAttack;
+	float AttackChargeAmount;
 };
-
