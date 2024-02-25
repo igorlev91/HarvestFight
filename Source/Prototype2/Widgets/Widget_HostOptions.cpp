56063b32d7ac5bacab3dafbc7b96b4bc22ec9fe2
Igor Levdansky <igorlevdansky91@gmail.com>
2024-02-25 21:06:30 +0300

    Host menu - Extra settings
    Settings for:
    game speed - hooked up
    Stealing from plots - not hooked up
    Fertiliser spawning - not hooked up
    Cement spawning - not hooked up
    Poison spawning - not hooked up
    
    Also reset short game time back to 3 mins (from 1 min testing)

--- Source/Prototype2/Widgets/Widget_HostOptions.cpp
+++ Source/Prototype2/Widgets/Widget_HostOptions.cpp
@@ -4,11 +4,41 @@
 #include "Widget_HostOptions.h"
 
 /*#include "SocketSubsystem.h"*/
+#include "Widget_OptionSelector.h"
 #include "Components/CheckBox.h"
+#include "Components/TextBlock.h"
 #include "Kismet/KismetStringLibrary.h"
 #include "Prototype2/GameInstances/PrototypeGameInstance.h"
 
 /*#include "JsonObjectConverter.h"*/
+
+void UWidget_HostOptions::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	/* Extra Settings Setup */
+	
+	/* Set game speed control */
+	GameLength_Control->OptionText->SetText(FText::FromString("Game Speed"));
+	UpdateGameSpeedText();
+
+	/* Set stealing control */
+	Stealing_Control->OptionText->SetText(FText::FromString("Stealing (from plots)"));
+	UpdateStealingText();
+
+	/* Set fertiliser control */
+	Fertiliser_Control->OptionText->SetText(FText::FromString("Fertiliser Spawn (if available)"));
+	UpdateFertiliserText();
+
+	/* Set cement control */
+	Cement_Control->OptionText->SetText(FText::FromString("Cement Spawn (if available)"));
+	UpdateCementText();
+
+	/* Set poison control */
+	Poison_Control->OptionText->SetText(FText::FromString("Poison Spawn (if available)"));
+	UpdatePoisonText();
+	
+}
 
 void UWidget_HostOptions::NativeOnInitialized()
 {
@@ -20,6 +50,43 @@ void UWidget_HostOptions::NativeConstruct()
 {
 	Super::NativeConstruct();
 	StartButton->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnHostPressed);
+
+	/* Extra Settings Setup */
+	
+	/* Game speed control buttons */
+	if (GameLength_Control)
+	{
+		GameLength_Control->ButtonLeft->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnGameSpeedControlLeftButtonPressed);
+		GameLength_Control->ButtonRight->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnGameSpeedControlRightButtonPressed);
+	}
+
+	/* Stealing control buttons */
+	if (Stealing_Control)
+	{
+		Stealing_Control->ButtonLeft->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnStealingControlButtonPressed);
+		Stealing_Control->ButtonRight->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnStealingControlButtonPressed);
+	}
+
+	/* Fertiliser control buttons */
+	if (Fertiliser_Control)
+	{
+		Fertiliser_Control->ButtonLeft->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnFertiliserControlButtonPressed);
+		Fertiliser_Control->ButtonRight->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnFertiliserControlButtonPressed);
+	}
+	
+	/* Cement control buttons */
+	if (Cement_Control)
+	{
+		Cement_Control->ButtonLeft->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnCementControlButtonPressed);
+		Cement_Control->ButtonRight->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnCementControlButtonPressed);
+	}
+
+	/* Poison control buttons */
+	if (Poison_Control)
+	{
+		Poison_Control->ButtonLeft->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnPoisonControlButtonPressed);
+		Poison_Control->ButtonRight->OnPressed.AddDynamic(this, &UWidget_HostOptions::OnPoisonControlButtonPressed);
+	}
 }
 
 void UWidget_HostOptions::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
@@ -37,6 +104,209 @@ void UWidget_HostOptions::OnHostPressed()
 
 	if(GetOwningPlayer()->HasAuthority())
 		GameInstance->bTeams = IsTeamsButton->IsChecked();
+}
+
+void UWidget_HostOptions::ResetDefaults()
+{
+	TempGameSpeed = DefaultGameSpeed;
+	UpdateGameSpeedText();
+	SetGameSpeedControl();
+	TempStealingSetting = DefaultStealingSetting;
+	UpdateStealingText();
+	SetStealingControl();
+	TempFertiliserSetting = DefaultFertiliserSetting;
+	UpdateFertiliserText();
+	SetFertiliserControl();
+	TempCementSetting = DefaultCementSetting;
+	UpdateCementText();
+	SetCementControl();
+	TempPoisonSetting = DefaultPoisonSetting;
+	UpdatePoisonText();
+	SetPoisonControl();
+}
+
+void UWidget_HostOptions::ResetSetting()
+{
+	TempGameSpeed = GameSpeed;
+	UpdateGameSpeedText();
+	SetGameSpeedControl();
+	TempStealingSetting = StealingSetting;
+	UpdateStealingText();
+	SetStealingControl();
+	TempFertiliserSetting = FertiliserSetting;
+	UpdateFertiliserText();
+	SetFertiliserControl();
+	TempCementSetting = CementSetting;
+	UpdateCementText();
+	SetCementControl();
+	TempPoisonSetting = PoisonSetting;
+	UpdatePoisonText();
+	SetPoisonControl();
+}
+
+void UWidget_HostOptions::OnGameSpeedControlLeftButtonPressed()
+{
+	switch (TempGameSpeed)
+	{
+	case EGameSpeed::SHORT:
+		{
+			TempGameSpeed = EGameSpeed::LONG;
+			break;
+		}
+	case EGameSpeed::MEDIUM:
+		{
+			TempGameSpeed = EGameSpeed::SHORT;
+			break;
+		}
+	case EGameSpeed::LONG:
+		{
+			TempGameSpeed = EGameSpeed::MEDIUM;
+			break;
+		}
+		default:
+		{
+			TempGameSpeed = EGameSpeed::MEDIUM;
+			break;
+		}
+	}
+
+	UpdateGameSpeedText();
+}
+
+void UWidget_HostOptions::OnGameSpeedControlRightButtonPressed()
+{
+	switch (TempGameSpeed)
+	{
+	case EGameSpeed::SHORT:
+		{
+			TempGameSpeed = EGameSpeed::MEDIUM;
+			break;
+		}
+	case EGameSpeed::MEDIUM:
+		{
+			TempGameSpeed = EGameSpeed::LONG;
+			break;
+		}
+	case EGameSpeed::LONG:
+		{
+			TempGameSpeed = EGameSpeed::SHORT;
+			break;
+		}
+	default:
+		{
+			TempGameSpeed = EGameSpeed::MEDIUM;
+			break;
+		}
+	}
+	UpdateGameSpeedText();
+}
+
+void UWidget_HostOptions::UpdateGameSpeedText()
+{
+	switch (TempGameSpeed)
+	{
+	case EGameSpeed::SHORT:
+		{
+			GameLength_Control->OptionValueText->SetText(FText::FromString("Short"));
+			break;
+		}
+	case EGameSpeed::MEDIUM:
+		{
+			GameLength_Control->OptionValueText->SetText(FText::FromString("Medium"));
+			break;
+		}
+	case EGameSpeed::LONG:
+		{
+			GameLength_Control->OptionValueText->SetText(FText::FromString("Long"));
+			break;
+		}
+	default:
+		{
+			GameLength_Control->OptionValueText->SetText(FText::FromString("Medium"));
+			break;
+		}
+	}
+}
+
+void UWidget_HostOptions::SetGameSpeedControl()
+{
+	GameSpeed = TempGameSpeed;
+}
+
+void UWidget_HostOptions::OnStealingControlButtonPressed()
+{
+	TempStealingSetting = !TempStealingSetting;
+	UpdateStealingText();
+}
+
+void UWidget_HostOptions::UpdateStealingText()
+{
+	if (TempStealingSetting)
+		Stealing_Control->OptionValueText->SetText(FText::FromString("On"));
+	else
+		Stealing_Control->OptionValueText->SetText(FText::FromString("Off"));
+}
+
+void UWidget_HostOptions::SetStealingControl()
+{
+	StealingSetting = TempStealingSetting;
+}
+
+void UWidget_HostOptions::OnFertiliserControlButtonPressed()
+{
+	TempFertiliserSetting = !TempFertiliserSetting;
+	UpdateFertiliserText();
+}
+
+void UWidget_HostOptions::UpdateFertiliserText()
+{
+	if (TempFertiliserSetting)
+		Fertiliser_Control->OptionValueText->SetText(FText::FromString("On"));
+	else
+		Fertiliser_Control->OptionValueText->SetText(FText::FromString("Off"));
+}
+
+void UWidget_HostOptions::SetFertiliserControl()
+{
+	FertiliserSetting = TempFertiliserSetting;
+}
+
+void UWidget_HostOptions::OnCementControlButtonPressed()
+{
+	TempCementSetting = !TempCementSetting;
+	UpdateCementText();
+}
+
+void UWidget_HostOptions::UpdateCementText()
+{
+	if (TempCementSetting)
+		Cement_Control->OptionValueText->SetText(FText::FromString("On"));
+	else
+		Cement_Control->OptionValueText->SetText(FText::FromString("Off"));
+}
+
+void UWidget_HostOptions::SetCementControl()
+{
+	CementSetting = TempCementSetting;
+}
+
+void UWidget_HostOptions::OnPoisonControlButtonPressed()
+{
+	TempPoisonSetting = !TempPoisonSetting;
+	UpdatePoisonText();
+}
+
+void UWidget_HostOptions::UpdatePoisonText()
+{
+	if (TempPoisonSetting)
+		Poison_Control->OptionValueText->SetText(FText::FromString("On"));
+	else
+		Poison_Control->OptionValueText->SetText(FText::FromString("Off"));
+}
+
+void UWidget_HostOptions::SetPoisonControl()
+{
+	PoisonSetting = TempPoisonSetting;
 }
 
 /*void UWidget_HostOptions::GetPublicIPAddress()
--- Source/Prototype2/Widgets/Widget_HostOptions.h
+++ Source/Prototype2/Widgets/Widget_HostOptions.h
@@ -20,6 +20,15 @@ public:
 	FString ip;
 };*/
 
+UENUM(BlueprintType)
+enum class EGameSpeed : uint8 
+{
+	SHORT,
+	MEDIUM,
+	LONG,
+	INDEX
+};
+
 UCLASS()
 class PROTOTYPE2_API UWidget_HostOptions : public UUserWidget, public IWidgetUtilityInterface
 {
@@ -27,6 +36,7 @@ class PROTOTYPE2_API UWidget_HostOptions : public UUserWidget, public IWidgetUti
 
 	/* Public Functions */
 public:
+	virtual void NativePreConstruct() override;
 	virtual void NativeOnInitialized() override;
 	virtual void NativeConstruct() override;
 	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
@@ -68,4 +78,108 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
 	class UButton* StartButton;
+
+
+	/* Extra Settings */
+public:
+
+	// Reset settings to defaults
+	UFUNCTION(BlueprintCallable)
+	void ResetDefaults();
+
+	// Reset settings to actual setting - non temp
+	UFUNCTION(BlueprintCallable)
+	void ResetSetting();
+	
+	// Game Speed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	class UWidget_OptionSelector* GameLength_Control;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameSpeed DefaultGameSpeed = EGameSpeed::MEDIUM;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameSpeed TempGameSpeed = EGameSpeed::MEDIUM;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EGameSpeed GameSpeed = EGameSpeed::MEDIUM;
+
+	UFUNCTION()
+	void OnGameSpeedControlLeftButtonPressed();
+	UFUNCTION()
+	void OnGameSpeedControlRightButtonPressed();
+	UFUNCTION()
+	void UpdateGameSpeedText();
+	UFUNCTION(BlueprintCallable)
+	void SetGameSpeedControl();
+
+	// Stealing - Whether players can steal from other plots
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	UWidget_OptionSelector* Stealing_Control;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool DefaultStealingSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool TempStealingSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool StealingSetting = true;
+
+	UFUNCTION()
+	void OnStealingControlButtonPressed();
+	UFUNCTION()
+	void UpdateStealingText();
+	UFUNCTION(BlueprintCallable)
+	void SetStealingControl();
+
+	// Fertiliser - Whether fertiliser spawns for maps that have it
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	UWidget_OptionSelector* Fertiliser_Control;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool DefaultFertiliserSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool TempFertiliserSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool FertiliserSetting = true;
+
+	UFUNCTION()
+	void OnFertiliserControlButtonPressed();
+	UFUNCTION()
+	void UpdateFertiliserText();
+	UFUNCTION(BlueprintCallable)
+	void SetFertiliserControl();
+
+	// Cement - Whether cement spawns for maps that have it
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	UWidget_OptionSelector* Cement_Control;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool DefaultCementSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool TempCementSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool CementSetting = true;
+
+	UFUNCTION()
+	void OnCementControlButtonPressed();
+	UFUNCTION()
+	void UpdateCementText();
+	UFUNCTION(BlueprintCallable)
+	void SetCementControl();
+
+	// Poison - Whether poison spawns for maps that have it
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
+	UWidget_OptionSelector* Poison_Control;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool DefaultPoisonSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool TempPoisonSetting = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool PoisonSetting = true;
+
+	UFUNCTION()
+	void OnPoisonControlButtonPressed();
+	UFUNCTION()
+	void UpdatePoisonText();
+	UFUNCTION(BlueprintCallable)
+	void SetPoisonControl();
 };
