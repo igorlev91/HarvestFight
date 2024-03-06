b7c26e7c623ac90b259762c8d5bd12c2ad7c1c61
Igor Levdansky <igorlevdansky91@gmail.com>
2024-03-06 21:46:14 +0300

    change lighting base class

---
+++ Source/Prototype2/Gameplay/SmiteCloud.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include "Prototype2/Gameplay/SmiteCloud.h"
+
+// Sets default values
+ASmiteCloud::ASmiteCloud()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASmiteCloud::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASmiteCloud::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
